f8fa0f6930a369c974b24ea2fc1c19d8581b9621
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-04-05 12:22:22 -0500

    Added two GET endpoints /index and /index2 to CustomerController

--- src/main/java/com/example/controller/CustomerController.java
+++ src/main/java/com/example/controller/CustomerController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/customers")
 public class CustomerController {
 
+    @GetMapping("/index")
+    public String index() {
+        return "Hello, Customer index";
+    }
 
+    @GetMapping("/index2")
+    public String index2() {
+        return "Hello, Customer index2";
+    }
 }
